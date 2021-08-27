1c80e66ad0df9a7f6d9ca5b7ea20428575029980
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-08-27 15:06:53 +0100

    Rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+calculate how many square units of water will be retained after it rains
+"""
+
+
+def rain(walls):
+    """
+    calculat how many square units of water
+    will be retained after it rains
+    """
+
+    if not len(walls):
+        return 0
+    size = len(walls)
+    water = 0
+    for i in range(size):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
