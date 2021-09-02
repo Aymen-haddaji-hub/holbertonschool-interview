8088a2a7e59b28b9c67c7115b4f0381970d255e3
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-09-02 17:07:47 +0100

    header file

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,14 @@
+
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Prototypes */
+int advanced_binary(int *array, size_t size, int value);
+void print_array(int array[], size_t first, size_t size);
+
+#endif /* SEARCH_ALGOS */
