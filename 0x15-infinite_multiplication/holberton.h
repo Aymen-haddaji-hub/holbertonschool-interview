b1b2ce4d79742ffc1a518e63f09634664cce25b4
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-10-11 16:31:35 +0100

    infint mul

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int _putchar(char c);
+int _strlen(char *);
+int is_digit(char *c);
+void multiply(char *s1, char *s2);
+
+#endif /* HOLBERTON_H */
