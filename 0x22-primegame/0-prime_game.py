3ab4900fecd743c48154392f80175d378ff0e135
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-15 20:45:34 +0100

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -3,27 +3,28 @@
 
 
 def isWinner(x, nums):
-    """
-    Function that returns the winner of the game"""
-
+    '''
+    Returns: name of the player that won the most rounds
+    x is the number of rounds and nums is an array of n
+    '''
     if not nums or x < 1:
         return None
-    max_num = max(nums)
-    tmp = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not tmp[i]:
+    max_n = max(nums)
+    filters = [True for _ in range(max(max_n + 1, 2))]
+    for i in range(2, int(pow(max_n, 0.5)) + 1):
+        if not filters[i]:
             continue
-        for j in range(i * i, max_num + 1, i):
-            tmp[j] = False
-    tmp[0] = tmp[1] = False
-    a = 0
-    for i in range(len(tmp)):
-        if tmp[i]:
-            a += 1
-        tmp[i] = a
+        for j in range(i * i, max_n + 1, i):
+            filters[j] = False
+    filters[0] = filters[1] = False
+    c = 0
+    for i in range(len(filters)):
+        if filters[i]:
+            c += 1
+        filters[i] = c
     p_1 = 0
-    for max_num in nums:
-        p_1 += tmp[max_num] % 2 == 1
+    for max_n in nums:
+        p_1 += filters[max_n] % 2 == 1
     if p_1 * 2 == len(nums):
         return None
     if p_1 * 2 > len(nums):
