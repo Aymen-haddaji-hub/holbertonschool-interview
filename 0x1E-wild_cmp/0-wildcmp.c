38f9ac171e3f1a0a3ab9e0a821b2b3e9e005c206
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-15 16:50:09 +0100

    wildcmp

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -10,33 +10,19 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	int i, j;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0' && s2[j] != '\0')
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	else if (*s2 == '*')
 	{
-		if (s1[i] == s2[j])
-		{
-			i++;
-			j++;
-		}
-		else if (s2[j] == '*')
-		{
-			j++;
-			while (s2[j] == '*')
-				j++;
-			while (s1[i] != s2[j] && s1[i] != '\0')
-				i++;
-		}
-		else
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
-	if (s1[i] == '\0' && s2[j] == '\0')
-		return (1);
-	if (s2[j] == '\0' && s1[i] != '\0')
-		return (0);
-	if (s2[j] == '\0' && s1[i] == '\0')
-		return (1);
+
 	return (0);
 }
